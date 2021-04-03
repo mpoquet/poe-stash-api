5419db96af484bd13c9859aa521ae4e5eaf18813
Millian Poquet <millian.poquet@inria.fr>
2021-04-03 22:02:09 +0200

    nix: add R shell

--- default.nix
+++ default.nix
@@ -1,14 +1,28 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz") {}
 }:
 
-pkgs.mkShell rec {
-  name = "pyenv";
-  buildInputs = [
-    pkgs.python3
-    pkgs.python3Packages.plotly
-    pkgs.python3Packages.pandas
-  ] ++ [
-    pkgs.python3Packages.pylint
-    pkgs.python3Packages.ipython
-  ];
-}
+let self = rec {
+  python-shell = pkgs.mkShell rec {
+    name = "python-shell";
+    buildInputs = [
+      pkgs.python3
+      pkgs.python3Packages.plotly
+      pkgs.python3Packages.pandas
+    ] ++ [
+      pkgs.python3Packages.pylint
+      pkgs.python3Packages.ipython
+    ];
+  };
+
+  r-shell = pkgs.mkShell rec {
+    name = "r-shell";
+    buildInputs = [
+      pkgs.R
+      pkgs.rPackages.tidyverse
+      pkgs.rPackages.viridis
+      pkgs.rPackages.treemap
+    ];
+  };
+};
+in
+  self
